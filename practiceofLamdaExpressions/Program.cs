b8dc92be7621a77aa3deb019bf41b7d650c3a66a
unknown <abhimvj575@gmail.com>
2022-12-27 06:25:12 +0530

    [Abhilash] Added: person data management

---
+++ practiceofLamdaExpressions/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace practiceofLamdaExpressions
+{
+    public class Program
+    {
+           public static void AddRecords(List<person> listperson)
+        {
+            listperson.Add(new person("1289", "Abhilash", 22, "bangalore"));
+            listperson.Add(new person("1234", "lokesh", 25, "chennai"));
+            listperson.Add(new person("4556", "anil", 30, "mumbai"));
+            listperson.Add(new person("5667", "mahesh", 60, "delhi"));
+            listperson.Add(new person("4778", "amruth", 60, "calicutt"));
+
+        }       
+       public  static void Main(string[] args)
+        {
+            List<person> listperson = new List<person>();           
+        }
+    }
+    
+}
---
+++ practiceofLamdaExpressions/person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceofLamdaExpressions
+{
+    public  class person
+    {
+        public string SSN;
+        public string name;
+        public int age;
+        public string address;
+
+        public person(string SSN,string name,int age,string address)
+        {
+            this.SSN = SSN;
+            this.name = name;
+            this.age = age;
+            this.address = address; 
+
+        }
+    }
+}
